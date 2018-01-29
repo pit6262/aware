637c1d7ad164f721e05103c80337c861b51b4d9c
pit <orel-1993@yandex.com>
2018-01-29 12:59:15 +0200

    Aware 1.4

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,15 @@
 $(function(){
 
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
+
+	
 
 });
 
