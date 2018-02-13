9713e88266b54a08f9fa15fd4e6426f2d2f1091a
pit <orel-1993@yandex.com>
2018-02-13 15:14:30 +0200

    Aware 1.5

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,27 @@ $(function(){
 		return false
 	});
 
+	$('.accordion__head').on('click', function(){
+		var el = $(this);
+		el.next('.accordion__body').slideToggle();
+		el.toggleClass('open');
+		return false;
+	});
+
+	$('.styler').styler();
+
+	$( "#slider-range" ).slider({
+		range: true,
+		min: 0,
+		max: 500,
+		values: [ 75, 300 ],
+		slide: function( event, ui ) {
+			$( "#amount" ).val( "$" + ui.values[ 0 ] );
+			$( "#amount1" ).val( "$" + ui.values[ 1 ] );
+		}
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
 	
 
 });
